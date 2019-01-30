e0cf918d7a38fb1170dc57b397fcc9a2f4cec6af
Fucksky <zhaohao1997@zju.edu.cn>
2019-01-30 22:03:41 +0800

    hw1 completed

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,16 +1,44 @@
 // TODO: Make sure to make this class a part of the synthesizer package
 // package <package name>;
+package synthesizer;
+import synthesizer.AbstractBoundedQueue;
+
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
-    private int first;            // index for the next dequeue or peek
+    private int first;
+    // index for the next dequeue or peek
     /* Index for the next enqueue. */
     private int last;
     /* Array for storing the buffer data. */
     private T[] rb;
+
+    private class IteratorHelper implements Iterator<T> {
+        private int wizardPosition = first;
+        public boolean hasNext() {
+            if (isEmpty()) {
+                return false;
+            } else if (first < last) {
+                return wizardPosition < last;
+            } else {
+                return wizardPosition < last + capacity;
+            }
+        }
+        public T next() {
+            T result = rb[wizardPosition % capacity];
+            wizardPosition++;
+            return result;
+        }
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return new IteratorHelper();
+    }
 
     /**
      * Create a new ArrayRingBuffer with the given capacity.
@@ -21,6 +49,9 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        rb = (T[]) new Object[capacity];
+        first = last = fillCount = 0;
+        this.capacity = capacity;
     }
 
     /**
@@ -30,6 +61,13 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        } else {
+            rb[last] = x;
+            last = (last + 1) % capacity;
+            fillCount++;
+        }
     }
 
     /**
@@ -38,7 +76,16 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        } else {
+            T result = rb[first];
+            rb[first] = null;
+            first = (first + 1) % capacity;
+            fillCount--;
+            return result;
+        }
     }
 
     /**
@@ -46,6 +93,7 @@ public class ArrayRingBuffer<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,6 +1,7 @@
 package synthesizer;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.Iterator;
 
 /** Tests the ArrayRingBuffer class.
  *  @author Josh Hug
@@ -9,11 +10,23 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer arb = new ArrayRingBuffer(10);
     }
 
     /** Calls tests for ArrayRingBuffer. */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
+    }
+
+    @Test
+    public void forTest() {
+        ArrayRingBuffer<String> fuck = new ArrayRingBuffer<>(10);
+        fuck.enqueue("MY");
+        fuck.enqueue("NAME");
+        fuck.enqueue("IS");
+        fuck.enqueue("FUCKSKY");
+        for (String s : fuck) {
+            System.out.println(s);
+        }
     }
 } 
